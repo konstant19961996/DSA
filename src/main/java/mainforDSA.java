8354bdf8db4a83f10f11523d0831c243982d0579
konstant19961996 <bks1906@gmail.com>
2017-09-14 19:18:56 +0300

    Update mainforDSA.java

--- src/main/java/mainforDSA.java
+++ src/main/java/mainforDSA.java
@@ -14,23 +14,13 @@ public class mainforDSA {
         String msg="Hello world!";
         BigInteger[] res = dsa.sign(msg);
         BigInteger r,s,z;
-        //z=BigInteger.valueOf(2152302898747);
-        z=new BigInteger("9999973");
-        r=res[0];
-        s=res[1];
-        /*KeyPairGenerator keyGen = KeyPairGenerator.getInstance("DSA");
+        KeyPairGenerator keyGen = KeyPairGenerator.getInstance("DSA");
         keyGen.initialize(512);
         KeyPair keyPair = keyGen.genKeyPair();
         DSAPrivateKey privateKey = (DSAPrivateKey) keyPair.getPrivate();
         DSAParams dsaParams1 = privateKey.getParams();
-        BigInteger p1 = dsaParams1.getP();*/
-        Miller_Rabin a = new Miller_Rabin();
-       // System.out.println("r="+r+"\ns="+s+"\n");
-       // System.out.println("Checking:"+dsa.verify(msg,r,s).toString());
-        System.out.println(z);
-        if (a.isPrime(z))
-            System.out.println("Число "+z+" простое");
-        else
-            System.out.println("Число "+z+" составное");
+        BigInteger p1 = dsaParams1.getP();
+        System.out.println("r="+r+"\ns="+s+"\n");
+        System.out.println("Checking:"+dsa.verify(msg,r,s).toString())
     }
 }
